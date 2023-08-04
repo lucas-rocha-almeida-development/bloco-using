99f56ae6733345c6363f48f0283727d0a682e862
Lucas Almeida <lucalmr12@gmail.com>
2023-08-04 19:03:21 -0300

    Adicionar arquivos de projeto.

---
+++ bloco using/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloco_using
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                string path = @"C:\Curso C\file01.txt";//recebendo arquivo do computador
+                                                       //using (FileStream fs = new FileStream(path, FileMode.Open))// abrindo arquivo do computador na tela
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) //fechando arquivo do computador
+                {
+                    //imprimindo na tela os dados do arquivo ate ultima linha
+                    while (!sr.EndOfStream)
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+
+
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("Erro apresentado ");
+                Console.WriteLine(e.Message);
+            }
+            
+            }
+            
+            
+    }
+}
